d610af7479e5aca530377ec09dfb4e49fc2604f4
anisbastide <anis.bastide@upslide.net>
2019-12-02 17:59:42 +0100

    kata lundi 02

---
+++ Kata_2/Kata_2/StringCalculator.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kata_2
+{
+    public class StringCalculator
+    {
+        public int Add(string number)
+        {
+            
+            if (number == "")
+            {
+                return 0;
+            }
+            string[] separatorForCustomStrings = {"/","\n"};
+            string[] separatorList= number.Split(separatorForCustomStrings, StringSplitOptions.RemoveEmptyEntries);
+            string separator1 = separatorList[0];
+            string[] separator = {",", "\n","/", separator1 };
+            string[] stringList = number.Split(separator, StringSplitOptions.RemoveEmptyEntries);
+            int result = 0;
+            List<int> intList = stringList.Select(int.Parse).ToList();
+            var count = 0;
+            foreach (int N in intList)
+            {
+
+                if (N < 0)
+                {
+                    count++;
+                    throw new ArgumentException("Pas de nombres negatif");
+                }
+                result = result + N;
+            }
+            return result;
+        }
+    }
+}
---
+++ Kata_2/TestKata_2/TestKata2.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Kata_2;
+using NUnit.Framework;
+
+namespace TestKata_2
+{
+    [TestFixture]
+    public class TestKata2
+    {
+        [TestCase("", 0)]
+        [TestCase("//,\n1", 1)]
+        [TestCase("//,\n1,2", 3)]
+        [TestCase("//,\n1,2,3,4,5", 15)]
+        [TestCase("//,\n1\n2\n3\n4\n5", 15)]
+        [TestCase("//;\n1;2", 3)]
+
+        public void TestStringCalculator(string number, int expectedResult)
+        {
+            //Arrange
+            var stringCalculator = new StringCalculator();
+
+            //Act
+            var actualResult = stringCalculator.Add(number);
+
+            //Assert
+            Assert.AreEqual(expectedResult, actualResult);
+
+        }
+        [Test]
+        public void TestException()
+        {
+            //Arrange
+            var stringCalculator = new StringCalculator();
+
+            //Act
+
+            //Assert
+            Assert.Throws<ArgumentException>(() => stringCalculator.Add(("//;\n1;2;-5")));
+
+        }
+    }
+}
