cefbf681286cbb33bac5ff576e076c6717b77c5c
anisbastide <anis.bastide@upslide.net>
2019-12-04 17:17:55 +0100

    First part of the kata, mercredi 04

---
+++ Kata_2/Kata_2/StringCalculator.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Specialized;
+using System.Linq;
+
+namespace Kata_2
+{
+    public class StringCalculator
+    {
+        public int Add(string number)
+        {
+            if (number == "")
+            {
+                return 0;
+            }
+
+            return Sum(number);
+        }
+
+        public int Sum(string number)
+        {
+            var customSeparator = "";
+            if (number.StartsWith("/"))
+            {
+                 customSeparator = CustomSeparator(number);
+            }
+            
+            String[] separator = {",","\n","/",customSeparator};
+            var stringNumbers = number.Split(separator, StringSplitOptions.RemoveEmptyEntries).ToList();
+            var numbers = stringNumbers.Select(int.Parse).ToList();
+            var sum = 0;
+            var count = 0;
+            var negativeNumbers = "";
+            foreach (int intNumber in numbers)
+            {
+                if (intNumber < 0)
+                {
+                    negativeNumbers+=intNumber + ";";
+                    count++;
+                }
+                sum += intNumber;
+            }
+
+            if (count > 0)
+            {
+                negativeNumbers=negativeNumbers.Remove((negativeNumbers.Length - 1));
+                throw new ArgumentException($"negatives not allowed, there is {count} negative number:{negativeNumbers}");
+            }
+            return sum;
+        }
+
+        public string CustomSeparator(string number)
+        {
+            String[] separator = { ",", "\n", "/" };
+            var stringNumbers = number.Split(separator, StringSplitOptions.RemoveEmptyEntries).ToList();
+            return stringNumbers[0];
+        }
+    }
+}
---
+++ Kata_2/TestKata_2/TestKata.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Kata_2;
+using NUnit.Framework;
+
+namespace TestKata_2
+{
+    [TestFixture]
+    public class TestKata
+    {
+        [TestCase("",0)]
+        [TestCase("1",1)]
+        [TestCase("1,2",3)]
+        [TestCase("1\n2",3)]
+        [TestCase("//;\n1;2",3)]
+        public void VerifyAddOutput(string number,int expectedResult)
+        {
+            //Arrange
+            var stringCalculator = new StringCalculator();
+            //Act
+            var actualResult = stringCalculator.Add(number);
+            //Assert
+            Assert.AreEqual(expectedResult,actualResult);
+        }
+        [TestCase("1,-2,-3,6")]
+        public void ThrowExceptionNegativeNumbers(string number)
+        {
+            //Arrange
+            var stringCalculator = new StringCalculator();
+            //Act
+            //Assert
+            Assert.Throws<ArgumentException>(() => stringCalculator.Add(number));
+        }
+    }
+}
