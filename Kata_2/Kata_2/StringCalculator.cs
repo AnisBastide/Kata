7424be43a7c913a729a22329df5a29dd47eec9a5
anisbastide <anis.bastide@upslide.net>
2019-12-05 11:54:25 +0100

    kata jeudi_05

---
+++ Kata_2/Kata_2/StringCalculator.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kata_2
+{
+    public class StringCalculator
+    {
+        public int Add(string number)
+        {
+            if (number == "")
+            {
+                return 0;
+            }
+
+            var sum = Sum(number);
+            return sum;
+        }
+
+        public int Sum(string number)
+        {
+            String[] separator = { "," };
+            var stringNumbers=number.Split(separator, StringSplitOptions.RemoveEmptyEntries);
+            var intNumbers = stringNumbers.Select(int.Parse).ToList();
+            var sum = 0;
+            foreach (int intNumber in intNumbers)
+            {
+                sum += intNumber;
+            }
+
+            return sum;
+        }
+    }
+}
---
+++ Kata_2/TestKata_2/TestKata.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Kata_2;
+using NUnit.Framework;
+
+namespace TestKata_2
+{
+    [TestFixture]
+    public class TestKata
+    {
+        [TestCase("",0)]
+        [TestCase("1",1)]
+        [TestCase("1,2",3)]
+        public void VerifyAddOutput(string number,int expectedResult)
+        {
+            //Arrange
+            var stringCalculator = new StringCalculator();
+            //Act
+            var actualResult = stringCalculator.Add(number);
+            //Assert
+            Assert.AreEqual(expectedResult,actualResult);
+        }
+    }
+}
