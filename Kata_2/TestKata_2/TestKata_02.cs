a02c776c9021e29e402e29328ff3491eb3696945
anisbastide <anis.bastide@upslide.net>
2019-12-06 17:23:32 +0100

    no message

---
+++ Kata_2/TestKata_2/TestKata_02.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Kata_2;
+using NUnit.Framework;
+using NUnit.Framework.Internal;
+
+namespace TestKata_2
+{
+    [TestFixture]
+    public class StringCalculatorTest
+    {
+        [TestCase("", 0)]
+        [TestCase("1", 1)]
+        [TestCase("1,2", 3)]
+        [TestCase("1,2,3,4", 10)]
+        [TestCase("1\n2", 3)]
+        [TestCase("//;\n1;2", 3)]
+        public void VerifyAddOutput(string numbers, int expectedResult)
+        {
+            //Arrange
+            var stringCalculator = new StringCalculator();
+            //Act
+            var actualResult = stringCalculator.Add(numbers);
+            //Assert
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+        [Test]
+        public void ThrowExceptionWhenAddNegativeNumber()
+        {
+            //Arrange
+            var number = "1,-2,-5";
+            var stringCalculator = new StringCalculator();
+            //Act
+            //Assert
+            Assert.Throws<ArgumentException>((() => stringCalculator.Add(number)));
+        }
+    }
+}
